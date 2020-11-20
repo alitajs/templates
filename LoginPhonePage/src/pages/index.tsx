3c722d24372ec109df45860aa21fc00cec3becd5
shezhiwei <1197530962@qq.com>
2020-11-20 11:09:44 +0800

    feat:add the LoginPhonePage

---
+++ LoginPhonePage/src/pages/index.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react';
+import styles from './index.less';
+import { List, InputItem, WingBlank, Button } from 'antd-mobile';
+
+interface PageProps {}
+
+const LoginPhonePage: FC<PageProps> = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>手机快捷登录</div>
+      <List className={styles.content}>
+        <InputItem clear placeholder="请输入手机号">
+          手机号
+        </InputItem>
+        <InputItem clear extra={<span>发送验证码</span>} placeholder="请输入验证码">
+          验证码
+        </InputItem>
+      </List>
+      <WingBlank size="md">
+        <Button type="primary" style={{ marginTop: '.4rem' }}>
+          登录
+        </Button>
+      </WingBlank>
+      <span className={styles.contentBottom}>账号密码登录</span>
+    </div>
+  );
+};
+
+export default LoginPhonePage;
