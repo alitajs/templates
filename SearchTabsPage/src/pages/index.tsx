c7c7e537d3d193aeb688f9497e85cc6cea3c34d1
shezhiwei <1197530962@qq.com>
2020-11-23 11:12:45 +0800

    feat:添加搜索结果页面选择卡类型-版本1页面

---
+++ SearchTabsPage/src/pages/index.tsx
@@ -0,0 +1,65 @@
+import React, { FC } from 'react';
+import styles from './index.less';
+import { List, SearchBar, Tabs } from 'antd-mobile';
+
+const { Item } = List;
+
+interface PageProps {}
+
+const SearchTabsPage: FC<PageProps> = () => {
+  const tabs = [
+    { title: '每日推荐' },
+    { title: '热门搜索' },
+    { title: '新上好课' },
+    { title: '新手入门' },
+  ];
+  const listData = [
+    {
+      id: 1,
+      content: '汉堡抢新品福利券',
+    },
+    {
+      id: 2,
+      content: '手机充值抽免单红包',
+    },
+    {
+      id: 3,
+      content: '厉害了我的国',
+    },
+    {
+      id: 4,
+      content: '蚂蚁森林',
+    },
+  ];
+  const ListTabsItem = () => {
+    return (
+      <List renderHeader="常用商家">
+        {listData.map((val) => (
+          <Item
+            key={val.id}
+            thumb="https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png"
+          >
+            {val.content}
+          </Item>
+        ))}
+      </List>
+    );
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <SearchBar placeholder="搜素你需要的商品热门关键词" />
+      </div>
+      <div className={styles.tabsContent}>
+        <Tabs tabs={tabs} tabBarActiveTextColor="#1677FF">
+          <ListTabsItem />
+          <ListTabsItem />
+          <ListTabsItem />
+          <ListTabsItem />
+        </Tabs>
+      </div>
+    </div>
+  );
+};
+
+export default SearchTabsPage;
