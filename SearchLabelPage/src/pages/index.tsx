03e2fda947eed0448bf2339c4050d611da1f27f9
shezhiwei <1197530962@qq.com>
2020-11-25 11:29:01 +0800

    chore:添加Flex.Item

--- SearchLabelPage/src/pages/index.tsx
+++ SearchLabelPage/src/pages/index.tsx
@@ -5,67 +5,33 @@ import styles from './index.less';
 interface PageProps {}
 
 const SearchLabelPage: FC<PageProps> = () => {
-  const tagTopData = [
-    {
-      id: 1,
-      text: '体育场馆预订',
-    },
-    {
-      id: 2,
-      text: '停车泊位',
-    },
-    {
-      id: 3,
-      text: '爱心捐献',
-    },
-    {
-      id: 4,
-      text: '蚂蚁借呗',
-    },
-  ];
-  const tagBottomData = [
-    {
-      id: 1,
-      text: '体育场馆预…',
-    },
-    {
-      id: 2,
-      text: '停车泊位',
-    },
-    {
-      id: 3,
-      text: '厉害了我的国',
-    },
-    {
-      id: 4,
-      text: '蚂蚁借呗',
-    },
-    {
-      id: 5,
-      text: '最多五个字…',
-    },
-  ];
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <SearchBar placeholder="搜素你需要的商品热门关键词" />
       </div>
       <List renderHeader="搜索历史" className={styles.gridStyle}>
-        <Flex wrap="wrap" justify="start">
-          {tagTopData.map((val) => (
-            <div className={styles.flexItemStyle} key={val.id}>
-              {val.text}
-            </div>
-          ))}
+        <Flex justify="around">
+          <Flex.Item>体育场馆预订</Flex.Item>
+          <Flex.Item>停车泊位</Flex.Item>
+          <Flex.Item>爱心捐献</Flex.Item>
+        </Flex>
+        <Flex justify="around">
+          <Flex.Item>蚂蚁借呗</Flex.Item>
+          <Flex.Item style={{ background: '#F5F5F5' }}></Flex.Item>
+          <Flex.Item style={{ background: '#F5F5F5' }}></Flex.Item>
         </Flex>
       </List>
       <List renderHeader="搜索发现" className={styles.gridStyle}>
-        <Flex wrap="wrap" justify="start">
-          {tagBottomData.map((val) => (
-            <div className={styles.flexItemStyle} key={val.id}>
-              {val.text}
-            </div>
-          ))}
+        <Flex justify="around">
+          <Flex.Item>体育场馆预…</Flex.Item>
+          <Flex.Item>停车泊位</Flex.Item>
+          <Flex.Item>厉害了我的国</Flex.Item>
+        </Flex>
+        <Flex justify="around">
+          <Flex.Item>蚂蚁借呗</Flex.Item>
+          <Flex.Item>最多五个字…</Flex.Item>
+          <Flex.Item style={{ background: '#F5F5F5' }}></Flex.Item>
         </Flex>
       </List>
     </div>
