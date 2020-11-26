7d0f8af64e9cf7aee3df86a011856485be33bfd7
xiaohuoni <448627663@qq.com>
2020-11-26 09:24:20 +0800

    chore: code style

--- ResultsTestPage/src/pages/index.tsx
+++ ResultsTestPage/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC } from 'react';
 import { Button, Result, WhiteSpace, WingBlank } from 'antd-mobile';
 import styles from './index.less';
 
@@ -8,27 +8,27 @@ interface PageProps {
 }
 
 const ResultsTestPage: FC<PageProps> = () => {
-    return (
-        <div className={styles.container}>
-          <div className={styles.headerResult}>
-            <Result
-              img={<img src={BgImg} style={{ width: '2rem', height: '2rem' }} alt="" />}
-              title="状态提示"
-              message="辅助说明"
-            />
-          </div>
-          <WhiteSpace size="sm" />
-          <WhiteSpace size="lg" />
-          <WingBlank size="lg">
-            <Button style={{ background: '#1677FF', color: '#FFFFFF' }}>按钮</Button>
-          </WingBlank>
-          <WhiteSpace size="sm" />
-          <WhiteSpace size="xs" />
-          <WingBlank size="lg">
-            <Button>按钮</Button>
-          </WingBlank>
-        </div>
-      );
+  return (
+    <div className={styles.container}>
+      <div className={styles.headerResult}>
+        <Result
+          img={<img src={BgImg} style={{ width: '2rem', height: '2rem' }} alt="" />}
+          title="状态提示"
+          message="辅助说明"
+        />
+      </div>
+      <WhiteSpace size="sm" />
+      <WhiteSpace size="lg" />
+      <WingBlank size="lg">
+        <Button style={{ background: '#1677FF', color: '#FFFFFF' }}>按钮</Button>
+      </WingBlank>
+      <WhiteSpace size="sm" />
+      <WhiteSpace size="xs" />
+      <WingBlank size="lg">
+        <Button>按钮</Button>
+      </WingBlank>
+    </div>
+  );
 };
 
 export default ResultsTestPage;
