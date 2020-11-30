fb643237e0add78e6233c85560727a2b5d7bdc4f
chenshuhang <437132124@qq.com>
2020-11-30 14:51:24 +0800

    feat: add CityPickerPage

--- CityPickerPage/src/pages/index.tsx
+++ CityPickerPage/src/pages/index.tsx
@@ -1,230 +1,143 @@
-import React, { FC } from 'react';
+/* eslint-disable no-console */
+import React, { FC, useState, useEffect, useRef } from 'react';
+import { SearchBar, List } from 'antd-mobile';
+import { pinyin } from 'pinyin-pro';
+import CityData from '../public/city.json';
+import { CityGrid, FirstCodeList, DataProps, FirstCodeProps } from '../components';
 import styles from './index.less';
-import { List, SearchBar, Icon, Flex } from 'antd-mobile';
-import Place from '../../assets/place.png';
 
 const { Item } = List;
+
 interface PageProps {}
 
 const CityPickerPage: FC<PageProps> = () => {
-  const tagTopData = [
-    {
-      id: 1,
-      text: '乌鲁木齐',
-    },
-    {
-      id: 2,
-      text: '北京',
-    },
-    {
-      id: 3,
-      text: '杭州',
-    },
-    {
-      id: 4,
-      text: '阿克苏地区…',
-    },
-    {
-      id: 5,
-      text: '上海',
-    },
-    {
-      id: 6,
-      text: '西安',
-    },
-    {
-      id: 7,
-      text: '最多五个字…',
-    },
-    {
-      id: 8,
-      text: '香港',
-    },
-    {
-      id: 9,
-      text: '日本',
-    },
-  ];
-  const tagBottomData = [
-    {
-      id: 1,
-      text: '鞍山',
-    },
-    {
-      id: 2,
-      text: '阿克苏地区',
-    },
-    {
-      id: 3,
-      text: '安庆',
-    },
-    {
-      id: 4,
-      text: '安康',
-    },
-    {
-      id: 5,
-      text: '阿里',
-    },
-    {
-      id: 6,
-      text: '安顺',
-    },
-    {
-      id: 7,
-      text: '阿拉善右旗',
-    },
-  ];
-  const gridData = [
-    {
-      id: 1,
-      text: '热',
-    },
-    {
-      id: 2,
-      text: 'A',
-    },
-    {
-      id: 3,
-      text: 'B',
-    },
-    {
-      id: 4,
-      text: 'C',
-    },
-    {
-      id: 5,
-      text: 'D',
-    },
-    {
-      id: 6,
-      text: 'E',
-    },
-    {
-      id: 7,
-      text: 'F',
-    },
-    {
-      id: 9,
-      text: 'G',
-    },
-    {
-      id: 10,
-      text: 'H',
-    },
-    {
-      id: 11,
-      text: 'I',
-    },
-    {
-      id: 12,
-      text: 'J',
-    },
-    {
-      id: 13,
-      text: 'K',
-    },
-    {
-      id: 14,
-      text: 'L',
-    },
-    {
-      id: 15,
-      text: 'M',
-    },
-    {
-      id: 16,
-      text: 'N',
-    },
-    {
-      id: 17,
-      text: 'O',
-    },
-    {
-      id: 18,
-      text: 'P',
-    },
-    {
-      id: 19,
-      text: 'Q',
-    },
-    {
-      id: 20,
-      text: 'R',
-    },
-    {
-      id: 21,
-      text: 'S',
-    },
-    {
-      id: 22,
-      text: 'T',
-    },
-    {
-      id: 23,
-      text: 'U',
-    },
-    {
-      id: 24,
-      text: 'V',
-    },
-    {
-      id: 25,
-      text: 'W',
-    },
-    {
-      id: 26,
-      text: 'X',
-    },
-    {
-      id: 27,
-      text: 'Y',
-    },
-    {
-      id: 28,
-      text: 'Z',
-    },
-    {
-      id: 29,
-      text: '#',
-    },
-  ];
+  const [searchValue, setSearchValue] = useState<string>(''); // 搜索输入框值
+  const [cityData, setCityData] = useState({}); // 封装后的数据
+  const [codeList, setCodeList] = useState<{ label: string | React.ReactNode; value: string }[]>(
+    [],
+  );
+
+  const nowCityRef = useRef();
+  const hotCityRef = useRef();
+
+  /**
+   * 页面初始化
+   */
+  useEffect(() => {
+    const cData = {};
+    CityData.forEach((item: any) => {
+      const firstCode = pinyin(item.name, { pattern: 'initial' }).trim()[0];
+      if (cData[firstCode]) {
+        cData[firstCode].push(item);
+      } else {
+        cData[firstCode] = [{ ...item, firstCode }];
+      }
+    });
+
+    const newData = {};
+    const firList: ((prevState: never[]) => never[]) | { label: string; value: string }[] = [];
+    firList.push({ label: '搜', value: 'search' });
+    firList.push({ label: '热', value: 'hot' });
+    Object.keys(cData)
+      .sort()
+      .forEach((key) => {
+        newData[key] = cData[key];
+        firList.push({
+          label: key.toUpperCase(),
+          value: key,
+        });
+      });
+    setCodeList(firList);
+    setCityData(newData);
+  }, []);
+
+  /**
+   * 城市列表点击事件
+   */
+  const cityClick = (e: DataProps) => console.log(e);
+
+  /**
+   * 字母点击事件
+   */
+  const codeClick = (e: FirstCodeProps) => {
+    const { value } = e;
+    const labelNode = document.getElementById(`code-${value}`);
+    if (labelNode) {
+      labelNode.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <SearchBar placeholder="健康理财" />
+      <div className={styles.searchBar} id="code-search">
+        <SearchBar
+          placeholder="健康理财"
+          value={searchValue}
+          onChange={(e: string) => setSearchValue(e)}
+          onSubmit={(e: string) => console.log(e)}
+          onCancel={() => setSearchValue('')}
+        />
       </div>
-      <List renderHeader="当前城市" className={styles.gridStyle}>
-        <Item className={styles.tagStyle}>
-          <div className={styles.flexItem}>
-            <img src={Place} /> 杭州
-          </div>
-        </Item>
-      </List>
-      <List renderHeader="搜索历史" className={styles.gridStyle}>
-        <Item className={styles.tagStyle}>
-          {tagTopData.map((val) => (
-            <div key={val.id} className={styles.flexItem}>
-              {val.text}
-            </div>
+      <div className={styles.cityContainer}>
+        <CityGrid
+          cRef={nowCityRef}
+          title="当前城市"
+          data={[{ id: 'hangzhou', name: '杭州' }]}
+          style={{ paddingTop: '0.24rem' }}
+          onClick={(e: DataProps) => {
+            console.log(e);
+            if (hotCityRef && hotCityRef.current) hotCityRef.current.clear();
+          }}
+          current
+        />
+        <CityGrid
+          cRef={hotCityRef}
+          id="code-hot"
+          title="热门城市"
+          data={[
+            { id: '1', names: '乌鲁木齐' },
+            { id: '2', names: '北京' },
+            { id: '3', names: '杭州' },
+            { id: '4', names: '阿克苏' },
+            { id: '5', names: '上海' },
+            { id: '6', names: '西安' },
+            { id: '7', names: '最多五个字最多五个字' },
+            { id: '8', names: '香港' },
+            { id: '9', names: '日本' },
+          ]}
+          style={{ paddingTop: '0.4rem' }}
+          alias={{
+            id: 'id',
+            name: 'names',
+          }}
+          onClick={(e: DataProps) => {
+            console.log(e);
+            if (nowCityRef && nowCityRef.current) nowCityRef.current.clear();
+          }}
+        />
+        <div className={styles.cityDataStyle}>
+          {Object.keys(cityData).map((code) => (
+            <React.Fragment key={code}>
+              <div id={`code-${code}`} className={styles.cityCode}>
+                {code.toUpperCase()}
+              </div>
+              <List>
+                {(cityData[code] || []).map((item: DataProps) => (
+                  <Item
+                    key={`${code}-${item?.id}`}
+                    onClick={() => {
+                      cityClick(item);
+                    }}
+                  >
+                    {item?.name}
+                  </Item>
+                ))}
+              </List>
+            </React.Fragment>
           ))}
-        </Item>
-      </List>
-      <List renderHeader="A" className={styles.bottomList}>
-        {tagBottomData.map((val) => (
-          <Item key={val.id}>{val.text}</Item>
-        ))}
-      </List>
-      <Flex direction="column" className={styles.rightList}>
-        <Flex.Item>
-          <Icon type="search" color="#999999" />
-        </Flex.Item>
-        {gridData.map((val) => (
-          <Flex.Item key={val.id} className={val.text === 'P' ? 'checkedStyle' : ''}>
-            {val.text}
-          </Flex.Item>
-        ))}
-      </Flex>
+        </div>
+      </div>
+      <FirstCodeList data={codeList} onClick={codeClick} />
     </div>
   );
 };
