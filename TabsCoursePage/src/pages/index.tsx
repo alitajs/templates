b4e5563f9b89b2c7b5d9f37e8d81b8448090f44e
shezhiwei <1197530962@qq.com>
2020-12-11 10:42:39 +0800

    feat: 添加教育课程分类页面

--- TabsCoursePage/src/pages/index.tsx
+++ TabsCoursePage/src/pages/index.tsx
@@ -2,10 +2,9 @@ import React, { FC } from 'react';
 import { Tabs, Carousel, List, Flex } from 'antd-mobile';
 import styles from './index.less';
 
-interface PageProps {}
+interface PageProps { }
 
-const BgImg =
-  'https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/1c66e30d15d2490db49471e43393af32~tplv-k3u1fbpfcp-watermark.image';
+const BgImg = 'https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/1c66e30d15d2490db49471e43393af32~tplv-k3u1fbpfcp-watermark.image';
 
 const TabsCoursePage: FC<PageProps> = () => {
   const carouselData = [1, 2, 3];
