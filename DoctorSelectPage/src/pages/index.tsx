45dd884511ac44e16243d3a8273cb4ac614f0d43
3eCat <793927991@qq.com>
2020-12-11 14:47:40 +0800

    feat:添加医疗行业医生卡片-我的医生

--- DoctorSelectPage/src/pages/index.tsx
+++ DoctorSelectPage/src/pages/index.tsx
@@ -1,23 +1,62 @@
 import React, { FC, useState, useEffect } from 'react';
-
 import styles from './index.less';
-
+import { Grid } from 'antd-mobile';
+import addImg from '../../assets/add.png'
 interface PageProps {
 }
-
-
 const DoctorSelectPage: FC<PageProps> = () => {
   const [loading, setLoading] = useState<boolean>(true);
-  
-
+  //模拟数据
+  const data = [
+    {
+      name: '徐玲',
+      subjects: '骨科',
+      icon: 'https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/1c66e30d15d2490db49471e43393af32~tplv-k3u1fbpfcp-watermark.image'
+    },
+    {
+      name: '庞然',
+      subjects: '男科',
+      icon: 'https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/1c66e30d15d2490db49471e43393af32~tplv-k3u1fbpfcp-watermark.image'
+    },
+    {
+      name: '迟慧艳',
+      subjects: '儿科',
+      icon: 'https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/1c66e30d15d2490db49471e43393af32~tplv-k3u1fbpfcp-watermark.image'
+    },
+    {
+      name: '梁晓军',
+      subjects: '内科',
+      icon: 'https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/1c66e30d15d2490db49471e43393af32~tplv-k3u1fbpfcp-watermark.image'
+    },
+    {
+      name: '添加',
+      subjects: '',
+      icon: addImg
+    }
+  ]
   return (
     <div className={styles.center}>
-      <div className={styles.headBtns}>
-      
-
+      <div className={styles.wrap}>
+        <Grid
+          className={styles.grid}
+          data={data}
+          columnNum={5}
+          hasLine={false}
+          square={false}
+          renderItem={dataItem => (
+            <div >
+              <img src={dataItem.icon} className={styles.gridItemImg} alt="" />
+              <div className={styles.gridItemName}>
+                <span>{dataItem.name}</span>
+              </div>
+              <div className={styles.gridItemSubjects}>
+                <span>{dataItem.subjects}</span>
+              </div>
+            </div>
+          )}
+        />
       </div>
     </div>
   );
 };
-
 export default DoctorSelectPage;
